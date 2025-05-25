b9266f5158bac3e7895668e4af808ea68578144f
Cristina-Medac <cam0028@alu.medac.es>
2025-05-25 03:13:00 +0200

    esto fufa ya

--- Proyecto JavaScript/js/script.js
+++ Proyecto JavaScript/js/script.js
@@ -2,72 +2,50 @@
 //VARIABLES UTILIZADAS
 let boton = document.getElementById('btn-añadir');
 let entrada = document.getElementById('nuevaTarea');
-let titulo;
-let tareas;
-let bloqueNT;
-let spanImportante;
-let spanTitulo;
-let spanX;
-let parrafo;
-let pOcultar;
-let infoExtra;
-let fecha;
-let caracteres;
-let tareasTerminadas;
-let textoCompleto;
-
-
-
 
 
 //FUNCIÓN PARA AÑADIR UNA TAREA NUEVA
 function añadirTarea(){
     if (entrada.value){
 
-        titulo = prompt("Introduce el título de la tarea");
+        let titulo = prompt("Introduce el título de la tarea");
 
         //bloque tareas
-        tareas = document.getElementById('tareas1');
-        bloqueNT = document.createElement('div');
+        let tareas = document.getElementById('tareas1');
+        let bloqueNT = document.createElement('div');
         tareas.appendChild(bloqueNT);
         bloqueNT.prioridad = false;
         bloqueNT.className = "tareas1";
 
-        //signo para cambiar prioridad
-        /*spanImportante = document.createElement('span');
-        spanImportante.innerText = '!';
-        bloqueNT.appendChild(spanImportante);
-        spanImportante.className = "importante";*/
-
         //titulo de la tarea
-        spanTitulo = document.createElement('span');
+        let spanTitulo = document.createElement('span');
         bloqueNT.appendChild(spanTitulo);
         spanTitulo.innerText = titulo;
         spanTitulo.className="nombre-tarea";
 
         //boton borrar tarea
-        spanX = document.createElement('span');
+        let spanX = document.createElement('span');
         spanX.innerHTML = 'X';
         bloqueNT.appendChild(spanX);
         spanX.className = "btn-borrar";
 
         //parrafo con la tarea completa
-        parrafo = document.createElement('p');
+        let parrafo = document.createElement('p');
         bloqueNT.appendChild(parrafo);
         parrafo.innerText = entrada.value;
         parrafo.className = "fulltext";
 
         //para la opcion de ocultar texto
-        pOcultar = document.createElement('span');
+        let pOcultar = document.createElement('span');
         pOcultar.innerHTML = 'Ocultar';
         bloqueNT.appendChild(pOcultar);
         pOcultar.className = "btn-ocultar";
 
         //apartado de la fecha y el número de caracteres
-        infoExtra = document.createElement('p');
+        let infoExtra = document.createElement('p');
         bloqueNT.appendChild(infoExtra);
-        fecha = document.createElement('span');
-        caracteres = document.createElement('span');
+        let fecha = document.createElement('span');
+        let caracteres = document.createElement('span');
         infoExtra.appendChild(fecha);
         infoExtra.appendChild(caracteres);
         fecha.innerHTML = 'fecha';
@@ -87,8 +65,11 @@ function añadirTarea(){
             
             if(getComputedStyle(parrafo).display === 'none'){
                 parrafo.className = "verTexto";
+                pOcultar.innerHTML = 'Ocultar';
             }else {
                 parrafo.className = "fulltext";
+                pOcultar.innerHTML = 'Mostrar';
+
             }
         })
         
@@ -101,31 +82,11 @@ function añadirTarea(){
 function eliminarTarea(laTarea){
     if(laTarea.parentNode.id ==='tareas2'){
         laTarea.remove();  
+
     }else {
-        tareasTerminadas = document.getElementById('tareas2');
+        let tareasTerminadas = document.getElementById('tareas2');
         tareasTerminadas.appendChild(laTarea);
         
     }
 }
 
-
-
-
-
-
-
-/*//lamo a la función de cambiar prioridad
-spanImportante.onclick = function (){
-    cambiarPrioridad(bloqueNT);
-}
-
-function cambiarPrioridad(bloqueImportante){
-    if(bloqueImportante.prioridad === false){
-        bloqueImportante.style.border = '4px solid rgb(195, 14, 14)';
-        bloqueImportante.prioridad = true;
-
-    }else {
-        bloqueImportante.style.border = "";
-        bloqueImportante.prioridad = false;
-    }
-}*/
