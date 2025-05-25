421449575cbc369e3fd56ec4ce20f1fb887cf95f
Cristina-Medac <cam0028@alu.medac.es>
2025-05-25 02:44:45 +0200

    JS Proyect - ocultar mostar check pero raro

--- Proyecto JavaScript/js/script.js
+++ Proyecto JavaScript/js/script.js
@@ -1,4 +1,5 @@
 
+//VARIABLES UTILIZADAS
 let boton = document.getElementById('btn-añadir');
 let entrada = document.getElementById('nuevaTarea');
 let titulo;
@@ -8,29 +9,35 @@ let spanImportante;
 let spanTitulo;
 let spanX;
 let parrafo;
-let ocultar;
+let pOcultar;
 let infoExtra;
 let fecha;
 let caracteres;
+let tareasTerminadas;
+let textoCompleto;
 
+
+
+
+
+//FUNCIÓN PARA AÑADIR UNA TAREA NUEVA
 function añadirTarea(){
-
-
     if (entrada.value){
 
         titulo = prompt("Introduce el título de la tarea");
 
         //bloque tareas
-        tareas = document.getElementById('tareas');
+        tareas = document.getElementById('tareas1');
         bloqueNT = document.createElement('div');
         tareas.appendChild(bloqueNT);
-        bloqueNT.className = "tareas";
+        bloqueNT.prioridad = false;
+        bloqueNT.className = "tareas1";
 
         //signo para cambiar prioridad
-        spanImportante = document.createElement('span');
+        /*spanImportante = document.createElement('span');
         spanImportante.innerText = '!';
         bloqueNT.appendChild(spanImportante);
-        spanImportante.className = "importante";
+        spanImportante.className = "importante";*/
 
         //titulo de la tarea
         spanTitulo = document.createElement('span');
@@ -48,12 +55,13 @@ function añadirTarea(){
         parrafo = document.createElement('p');
         bloqueNT.appendChild(parrafo);
         parrafo.innerText = entrada.value;
+        parrafo.className = "fulltext";
 
         //para la opcion de ocultar texto
-        ocultar = document.createElement('span');
-        ocultar.innerHTML = 'Ocultar';
-        bloqueNT.appendChild(ocultar);
-        ocultar.className = "btn-ocultar";
+        pOcultar = document.createElement('span');
+        pOcultar.innerHTML = 'Ocultar';
+        bloqueNT.appendChild(pOcultar);
+        pOcultar.className = "btn-ocultar";
 
         //apartado de la fecha y el número de caracteres
         infoExtra = document.createElement('p');
@@ -65,12 +73,59 @@ function añadirTarea(){
         fecha.innerHTML = 'fecha';
         caracteres.innerHTML = 'caracteres';
         infoExtra.className = "info-extra";
-
+        
         entrada.value = "";
-
+        
+        //llamada a la función para borrar tarea
+        spanX.onclick = function(){
+            eliminarTarea(bloqueNT);
+        }
+        
+        
+        //FUNCIÓN PARA MOSTRAR Y OCULTAR
+        pOcultar.addEventListener('click', function(){
+            
+            if(getComputedStyle(parrafo).display === 'none'){
+                parrafo.className = "verTexto";
+            }else {
+                parrafo.className = "fulltext";
+            }
+        })
+        
     }else{
         alert("La tarea no puede estar vacía");
     }
 };
 
+//FUNCIÓN PARA ELIMINAR LA TAREA
+function eliminarTarea(laTarea){
+    if(laTarea.parentNode.id ==='tareas2'){
+        laTarea.remove();  
+    }else {
+        tareasTerminadas = document.getElementById('tareas2');
+        tareasTerminadas.appendChild(laTarea);
+        
+    }
+}
 
+
+
+
+
+
+
+/*//lamo a la función de cambiar prioridad
+spanImportante.onclick = function (){
+    cambiarPrioridad(bloqueNT);
+}
+
+function cambiarPrioridad(bloqueImportante){
+    if(bloqueImportante.prioridad === false){
+        bloqueImportante.style.border = '4px solid rgb(195, 14, 14)';
+        bloqueImportante.prioridad = true;
+
+    }else {
+        bloqueImportante.style.border = "";
+        bloqueImportante.prioridad = false;
+    }
+}*/
